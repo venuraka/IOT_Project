d018d16357724e8f8d7a7531aaef12dd0ac6d7fe
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2025-02-08 16:35:46 +0530

    Create Firebase_Connection.ino

---
+++ Arduino/Firebase_Connection.ino
@@ -0,0 +1,10 @@
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
+// Define Firebase Data object
+
+#Define FIREBASE_HOST "https://fleetmanagement-dc58b-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#Define FIREBASE_AUTH "AIzaSyAC3QqZUaLVgkwLTw17GaFVaH9EWYqQGOg"
+#Define WIFI_SSID "your wifi ssid"
+#Define WIFI_PASSWORD "your wifi password"
+
